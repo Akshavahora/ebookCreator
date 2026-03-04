2159f6826ee83f12cb305afd9355ede731537d91
Aksha Vahora <vahoraakshu@gmail.com>
2026-03-04 10:26:35 +0530

    Import links use add logic of authentication in ProtectedRoute

--- frontend/eBookCreator/src/App.jsx
+++ frontend/eBookCreator/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import EditorPage from "./pages/EditorPage";
+import ViewBookpage from "./pages/ViewBookpage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center text-5xl">
-      App
+    <div>
+      <Routes>
+        {/* Public Rooutes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+        <Route
+          path="/editor/:bookId"
+          element={<ProtectedRoute><EditorPage /></ProtectedRoute>} />
+        <Route
+          path="/view-book/:bookId"
+          element={<ProtectedRoute><ViewBookpage /></ProtectedRoute>} />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      </Routes>
     </div>
   )
 }
--- frontend/eBookCreator/src/components/auth/ProtectedRoute.jsx
+++ frontend/eBookCreator/src/components/auth/ProtectedRoute.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+
+const ProtectedRoute = ({children}) => {
+  // Here you would typically check if the user is authenticated
+  const isAuthenticated = true; // Replace with actual authentication logic
+  const loading = false; // Replace with actual loading state
+  const location = useLocation(); 
+
+  if(loading) {
+    // You can return a loading spinner or placeholder here
+    return <div>Loading...</div>;
+  }
+
+  if(!isAuthenticated) {
+    return <Navigate to="/login" state={{from: location}} replace />;
+  }
+
+  return children;
 }
-
 export default ProtectedRoute
