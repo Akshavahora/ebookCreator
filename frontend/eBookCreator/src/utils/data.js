e9fc500cdf6e6f111af2d34359db1b115321ff74
Aksha Vahora <vahoraakshu@gmail.com>
2026-06-17 14:53:43 +0530

    create data.js file for features and testimonials

---
+++ frontend/eBookCreator/src/utils/data.js
@@ -0,0 +1,52 @@
+import { Lightbulb, BookOpen, Download, Library } from "lucid-react";
+
+export const FEATURES = [
+    {
+        title: "AI-Powered Writing",
+        description: "Overcome Writer's block with our smart assistant that helps you generate ideas, outlines, and content.",
+        icon: Lightbulb,
+        gradient: "from-violet-500 to-purple-600",
+    },
+    {
+        title: "Immersive Reader",
+        description: "Preview you ebook in a clean, read-only format. Adjust font sizes for a comfortable reading experience before you export.",
+        icon: BookOpen,
+        gradient: "from-blue-500 to-cyan-600",
+    },
+    {
+        title: "One-Click Export",
+        description: "Export your eBook to PDF and DOCX formats instantly, ready for publishing",
+        icon: Download,
+        gradient: "from-emerald-500 to-teal-600",
+    },
+    {
+        title: "eBook Management",
+        description: "Organize all your projects in a personal dashboard. Easily track progress, edit drafts and manage your library.",
+        icon: Library,
+        gradient: "from-pink-500 to-rose-600",
+    },
+];
+
+export const TESTIMONIALS = [
+    {
+        quote: 'This platform made it so easy to write and public my first ebook. The AI assistant is a game-changer!',
+        author: 'Jane Doe',
+        title: 'Bestselling Author',
+        avatar: 'https://images.unsplash.com/photo-1654110455429-cf322b40a906?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww',
+        rating: 5,
+    },
+    {
+        quote: 'I love the customize templates. I was able to create a beautiful ebook that matches my brand perfectly.',
+        author: 'Jane Smith',
+        title: 'Marketing Expert',
+        avatar: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fHww',
+        rating: 5,
+    },
+    {
+        quote: 'The one-click export feature saved me so much time. I was able to publish my ebook on multiple platforms in minute.',
+        author: 'Peter Jones',
+        title: 'Indian Publisher',
+        avatar: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww',
+        rating: 5,
+    },
+];
