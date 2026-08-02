581074fbbee358ecc9ca8b3fc573b12ed52d50dc
Aksha Vahora <vahoraakshu@gmail.com>
2026-08-02 15:21:22 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,10 @@ app.use("/api/export", exportRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
