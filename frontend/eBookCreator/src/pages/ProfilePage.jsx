d0a1a6e788ab692586ffea974a3c82d32c25e2be
Aksha Vahora <vahoraakshu@gmail.com>
2026-08-02 15:47:58 +0530

    axiosInstance updated file name in the import

--- frontend/eBookCreator/src/pages/ProfilePage.jsx
+++ frontend/eBookCreator/src/pages/ProfilePage.jsx
@@ -5,7 +5,7 @@ import DashboardLayout from "../components/layout/DashboardLayout";
 import InputField from "../components/ui/InputField";
 import Button from "../components/ui/Button";
 import { useAuth } from "../context/AuthContext";
-import axiosInstance from "../utils/AxiosInstance";
+import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPath";
 import toast from "react-hot-toast";
 
